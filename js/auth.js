91a6e656c9ad89de2b2256618db2a2100399014a
VIBHA <artvibha2007@gmail.com>
2026-03-25 23:11:12 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,19 @@
 // js/auth.js
+
+import { auth, db } from "./firebase.js";
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import {
+  setDoc,
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// SIGNUP
 export async function signup() {
   try {
     const email = document.getElementById("email").value;
@@ -19,50 +34,27 @@ export async function signup() {
     alert(error.message);
   }
 }
-import { auth, db } from "./firebase.js";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-import {
-  setDoc,
-  doc,
-  getDoc
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-// SIGNUP
-window.signup = async function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const role = document.getElementById("role").value;
-
-  const userCred = await createUserWithEmailAndPassword(auth, email, password);
-
-  await setDoc(doc(db, "users", userCred.user.uid), {
-    email: email,
-    role: role
-  });
-
-  alert("Signup successful!");
-  window.location.href = "login.html";
-};
 
 // LOGIN
-window.login = async function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+export async function login() {
+  try {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-  const userCred = await signInWithEmailAndPassword(auth, email, password);
+    const userCred = await signInWithEmailAndPassword(auth, email, password);
 
-  const docRef = doc(db, "users", userCred.user.uid);
-  const userData = await getDoc(docRef);
+    const docRef = doc(db, "users", userCred.user.uid);
+    const userData = await getDoc(docRef);
 
-  const role = userData.data().role;
+    const role = userData.data().role;
 
-  if (role === "fresher") {
-    window.location.href = "fresher.html";
-  } else {
-    window.location.href = "experienced.html";
+    if (role === "fresher") {
+      window.location.href = "fresher.html";
+    } else {
+      window.location.href = "experienced.html";
+    }
+
+  } catch (error) {
+    alert(error.message);
   }
-};
+}
